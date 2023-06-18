8e927d0d6a063ad9e38c4b47ad70499eb92e0307
John Lusk <john-github@how-hard-can-it-be.com>
2023-06-18 19:00:01 -0400

    Moved some stuff around.

--- main.go
+++ main.go
@@ -17,12 +17,13 @@ func main() {
 		panic("aaaaaaaaaaaaaa!")
 	})
 
-	r.Static("/web/dist/journal", "./web/dist/journal") // Serve a static base directory under the given path.
+	r.Static("/ng", "./ng/dist/journal") // Serve a static base directory under the given path.
+	r.Static("/static", "./static")      // Serve a static base directory under the given path.
 
-	// All weird routes need a default fallback, per
+	// All weird routes (in Angular app) need a default fallback, per
 	// https://angular.io/guide/deployment#routed-apps-must-fall-back-to-indexhtml
 	r.NoRoute(func(c *gin.Context) {
-		c.Redirect(302, "/web/dist/journal/index.html")
+		c.Redirect(302, "/ng/index.html")
 	})
 	r.Run()
 	// listen and serve on 0.0.0.0:8080
